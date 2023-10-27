9fcee5df01299ba17682cff24536bec95d28433e
Alex <akotenko@prezna.com>
2023-10-26 21:49:16 -0400

    2610L_Exercises: GettingStarted

---
+++ src/com/kotenko/exercises/GettingStarted.java
@@ -0,0 +1,20 @@
+package com.kotenko.exercises;
+
+public class GettingStarted {
+    public static void main(String[] args) {
+//        System.out.println("I can compile and run from the terminal. Hooray \uD83C\uDF89");
+
+//        System.out.println("I love cats");
+//        System.out.println("I hate cats");
+
+//        int number1 = 1;
+//        int number2 = 2;
+//        if (number1 > number2) {
+//            System.out.println("number1 > number2");
+//        } else if (number1 < number2) {
+//            System.out.println("number1 < number2");
+//        } else {
+//            System.out.println("number1 = number2");
+//        }
+    }
+}
