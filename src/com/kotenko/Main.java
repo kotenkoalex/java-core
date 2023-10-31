377fa018695eb18750e72f6b311e36211b8df41e
Alex <akotenko@prezna.com>
2023-10-31 17:23:08 -0400

    2610L_Exercises: ArraysConditionals and Methods

--- src/com/kotenko/Main.java
+++ src/com/kotenko/Main.java
@@ -1,7 +1,16 @@
 package com.kotenko;
+
+import java.util.Arrays;
+
 public class Main {
-    public static String  foo = "bar";
+
     public static void main(String[] args) {
-        String foo = "bar";
+        System.out.println("Program was given: ");
+        if (args.length > 0) {
+            System.out.println("The following arguments");
+            System.out.println(Arrays.toString(args));
+        } else {
+            System.out.println("0 argument");
+        }
     }
 }
