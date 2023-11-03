6487033e1545afec9024b46cd1ad390471093641
Alex <akotenko@prezna.com>
2023-11-02 20:37:33 -0400

    2610L_Exercises: Dates

--- src/com/kotenko/Main.java
+++ src/com/kotenko/Main.java
@@ -1,14 +1,7 @@
 package com.kotenko;
 
 public class Main {
-
     public static void main(String[] args) {
-        int balance = 100;
-        double balanceInDouble = balance;
-        System.out.println(balanceInDouble);
-
-        double ramainingBalance = 100.55;
-        int ramainingBalanceInt = (int) ramainingBalance;
-        System.out.println(ramainingBalanceInt);
+        String name = "Jamila";
     }
 }
