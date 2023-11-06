6a05f967ce2fd371606eb205bfa1d64ba6a261cb
Alex <akotenko@prezna.com>
2023-11-05 21:18:43 -0500

    2610L_Exercises: Exception Handling

--- src/com/kotenko/Main.java
+++ src/com/kotenko/Main.java
@@ -1,7 +1,20 @@
 package com.kotenko;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Jamila";
+        System.out.println("Hello what is your name?");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("Hello: " + input);
+
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
+        if(age < 15){
+            System.out.println("Your are a child");
+        } else {
+            System.out.println("Your are an adult");
+        }
     }
 }
