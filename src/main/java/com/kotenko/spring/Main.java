db895598c8db181698f89dbd5bc63c9eb4eac861
Alex <akotenko@prezna.com>
2023-12-14 21:52:30 -0500

    0312L_spring: added custom bean, custom exception

--- src/main/java/com/kotenko/spring/Main.java
+++ src/main/java/com/kotenko/spring/Main.java
@@ -2,10 +2,19 @@ package com.kotenko.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    public Foo getFoo() {
+        return new Foo("foo");
+    }
+
+    record Foo(String name) {
     }
 }
